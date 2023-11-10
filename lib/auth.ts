04e0636ddc8c4badc370baa095fe7f8ea8246017
Ricky Bruno <ricardobruno_89@hotmail.com>
2023-11-10 17:06:30 -0300

    working on auth

--- lib/auth.ts
+++ lib/auth.ts
@@ -19,49 +19,18 @@ export const authOptions: NextAuthOptions = {
     error: "/login", // Error code passed in query string as ?error=
   },
   session: { strategy: "jwt" },
-  cookies: {
-    sessionToken: {
-      name: `${VERCEL_DEPLOYMENT ? "__Secure-" : ""}next-auth.session-token`,
-      options: {
-        httpOnly: true,
-        sameSite: "lax",
-        path: "/",
-        // When working on localhost, the cookie domain must be omitted entirely (https://stackoverflow.com/a/1188145)
-        domain: VERCEL_DEPLOYMENT
-          ? `.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`
-          : undefined,
-        secure: VERCEL_DEPLOYMENT,
-      },
-    },
-  },
-  callbacks: {
-    jwt: async ({ token, user }) => {
-      if (user) {
-        token.user = user;
-      }
-      return token;
-    },
-    session: async ({ session, token }) => {
-      session.user = {
-        ...session.user,
-        // @ts-expect-error
-        id: token.sub,
-        // @ts-expect-error
-        username: token?.user?.username || token?.user?.gh_username,
-      };
-      return session;
-    },
-  },
-};
+  // callbacks: {
 
-export function getSession() {
-  return getServerSession(authOptions) as Promise<{
-    user: {
-      id: string;
-      name: string;
-      username: string;
-      email: string;
-      image: string;
-    };
-  } | null>;
-}
+  //   authorized({ auth, request: { nextUrl } }) {
+  //     const isLoggedIn = !!auth?.user;
+  //     const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+  //     if (isOnDashboard) {
+  //       if (isLoggedIn) return true;
+  //       return false; // Redirect unauthenticated users to login page
+  //     } else if (isLoggedIn) {
+  //       return Response.redirect(new URL('/dashboard', nextUrl));
+  //     }
+  //     return true;
+  //   },
+  // },
+};
