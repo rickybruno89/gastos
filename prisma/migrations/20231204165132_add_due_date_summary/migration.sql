0d7225a2118c2dacf342d409b79329f0dd2ba8c9
Ricky Bruno <ricardobruno_89@hotmail.com>
2023-12-04 13:52:14 -0300

    add dueDate

---
+++ prisma/migrations/20231204165132_add_due_date_summary/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "CreditCardPaymentSummary" ADD COLUMN     "dueDate" TEXT NOT NULL DEFAULT '2023-12-07';
---
+++ prisma/migrations/20231204165156_remove_default_falue/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "CreditCardPaymentSummary" ALTER COLUMN "dueDate" DROP DEFAULT;
