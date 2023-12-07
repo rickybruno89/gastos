cf1d2294956d826d570fe9430f728e3d97dcc645
Ricky Bruno <ricardobruno_89@hotmail.com>
2023-12-07 10:59:11 -0300

    fix loading

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -3,7 +3,7 @@ import { getToday } from '@/lib/utils'
 import DashboardTemplate from './_components/dashboard-template'
 import { Suspense } from 'react'
 import MonthSelector from './_components/month-selector'
-import { DashboardSkeleton } from './_components/skeletons/dashboard-skeleton'
+import LoadingSpinner from '@/components/ui/loading-spinner'
 
 export const metadata: Metadata = {
   title: 'Dashboard',
@@ -18,9 +18,16 @@ export default async function Page({
 }) {
   return (
     <main>
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-4 mb-20">
         <MonthSelector />
-        <Suspense key={`date=${searchParams.date}`} fallback={<DashboardSkeleton />}>
+        <Suspense
+          key={`date=${searchParams.date}`}
+          fallback={
+            <div className="mt-32">
+              <LoadingSpinner />
+            </div>
+          }
+        >
           <DashboardTemplate date={searchParams.date || getToday()} />
         </Suspense>
       </div>
