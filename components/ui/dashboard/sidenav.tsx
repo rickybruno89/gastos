61c00c8f04fd55b106af7f752428326d2b0c92d8
Ricky Bruno <ricardobruno_89@hotmail.com>
2023-12-07 17:16:13 -0300

    fix priority avatar

--- components/ui/dashboard/sidenav.tsx
+++ components/ui/dashboard/sidenav.tsx
@@ -1,19 +1,18 @@
-import NavLinks from '@/components/ui/dashboard/nav-links';
-import Image from 'next/image';
-import { getServerSession } from 'next-auth';
-import SignOutButton from './sign-out-button';
-import { nextAuthOptions } from '@/lib/auth';
+import NavLinks from '@/components/ui/dashboard/nav-links'
+import Image from 'next/image'
+import { getServerSession } from 'next-auth'
+import SignOutButton from './sign-out-button'
+import { nextAuthOptions } from '@/lib/auth'
 
 export default async function SideNav() {
-
   const session = await getServerSession(nextAuthOptions)
 
   return (
     <div className="flex h-full flex-col px-3 py-2 md:px-2">
       <div className="mb-2 flex h-20 flex-row md:flex-col items-center justify-center rounded-md bg-blue-600 p-4 md:h-40 gap-4">
-        <span className='text-white'>¡Hola!, {session?.user?.name}</span>
-        <div className='rounded-full overflow-hidden w-16 h-16' >
-          <Image width={100} height={100} src={session?.user?.image as string} alt="profile picture" />
+        <span className="text-white">¡Hola!, {session?.user?.name}</span>
+        <div className="rounded-full overflow-hidden w-16 h-16">
+          <Image priority width={100} height={100} src={session?.user?.image as string} alt="profile picture" />
         </div>
       </div>
       <div className="flex grow flex-row justify-between gap-2 md:flex-col md:space-x-0 md:space-y-2 flex-wrap">
@@ -22,5 +21,5 @@ export default async function SideNav() {
         <SignOutButton />
       </div>
     </div>
-  );
+  )
 }
