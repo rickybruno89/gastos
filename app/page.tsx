8f47d60dbea56701507bd2b02d2f993f0f511446
Ricky Bruno <ricardobruno_89@hotmail.com>
2023-12-13 13:53:13 -0300

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default async function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        <h1 className="font-bold text-white text-4xl">Bienvenido a GastApp</h1>
+        <h1 className="font-bold text-white text-4xl">¡Bienvenido a GastApp!</h1>
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
