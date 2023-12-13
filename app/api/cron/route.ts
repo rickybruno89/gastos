4e315b21b6975f72639d70dfe0316b4b1de3df4e
Ricky Bruno <ricardobruno_89@hotmail.com>
2023-12-13 14:43:35 -0300

    fix

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -4,8 +4,6 @@ import { headers } from 'next/headers'
 export async function GET() {
   const headersList = headers()
   const token = headersList.get('Authorization')
-  console.log('🚀 ~ file: route.ts:7 ~ GET ~ token:', token)
-  console.log('🚀 ~ file: route.ts:10 ~ GET ~ process.env.CRON_SECRET:', process.env.CRON_SECRET)
   if (token !== `Bearer ${process.env.CRON_SECRET}`) {
     return new Response('Unauthorized', {
       status: 401,
