120ab30f5d8d0dfdfd3ac543fe3fcd21011b6aaf
Ricky Bruno <ricardobruno_89@hotmail.com>
2024-08-09 18:08:35 -0300

    working on

--- app/(dashboard)/dashboard/_components/expenses-summary copy.tsx
+++ app/(dashboard)/dashboard/_components/expenses-summary copy.tsx
@@ -170,36 +170,6 @@ export default function ExpensesSummary({
     }
   }
 
-  const getCreditCardsTotal = () => {
-    const ccExpenses = creditCardExpenseSummaries.reduce(
-      (acc, expense) => {
-        if (expense.paid) return { ...acc, paid: acc.paid + expense.amount }
-        return { ...acc, notPaid: acc.notPaid + expense.amount }
-      },
-      { paid: 0, notPaid: 0 }
-    )
-    return {
-      paid: ccExpenses.paid,
-      notPaid: ccExpenses.notPaid,
-      amount: creditCardExpenseSummaries.reduce((acc, exp) => (acc += exp.amount), 0),
-    }
-  }
-
-  const getExpensesTotal = () => {
-    const expenses = expenseSummaries.reduce(
-      (acc, expense) => {
-        if (expense.paid) return { ...acc, paid: acc.paid + expense.amount }
-        return { ...acc, notPaid: acc.notPaid + expense.amount }
-      },
-      { paid: 0, notPaid: 0 }
-    )
-    return {
-      paid: expenses.paid,
-      notPaid: expenses.notPaid,
-      amount: expenseSummaries.reduce((acc, exp) => (acc += exp.amount), 0),
-    }
-  }
-
   const getNewExpenses = () =>
     expenses
       .filter((expense) => !expenseSummaries.some((expenseSummary) => expenseSummary.expenseId === expense.id))
@@ -226,20 +196,144 @@ export default function ExpensesSummary({
       ))
 
   return (
-    <section id="expense-content" className='max-w-xl mx-auto'>
-      <div className="flex gap-2 mb-4">
-        <div className="p-4 flex-1 flex flex-col aspect-video rounded-xl bg-gradient-to-bl from-violet-600 to-purple-600 text-white leading-tight">
-          <span className="text-lg font-semibold uppercase">total</span>
-          <span className="text-gray-100 uppercase">gastos fijos</span>
-          <span className="text-3xl font-bold mt-3 text-center">{formatCurrency(getExpensesTotal().amount)}</span>
-        </div>
-        <div className="p-4 flex-1 flex flex-col aspect-video rounded-xl bg-gradient-to-bl from-amber-500 to-orange-500 text-white">
-          <span className="text-lg font-semibold uppercase">TOTAL</span>
-          <span className="text-gray-100 uppercase">Tarjetas crédito</span>
-          <span className="text-3xl font-bold mt-3 text-center">{formatCurrency(getCreditCardsTotal().amount)}</span>
-        </div>
-      </div>
+    <section id="expense-content">
+      
       {expenseSummaries.length ? (
+        // <Table className="whitespace-nowrap">
+        //   <TableHeader>
+        //     <TableRow className="text-xs md:text-sm">
+        //       <TableHead className="sticky left-0 bg-white">Descripción</TableHead>
+        //       <TableHead>Forma de pago</TableHead>
+        //       <TableHead>Canal de pago</TableHead>
+        //       <TableHead>Fecha de vencimiento</TableHead>
+        //       <TableHead>Estado</TableHead>
+        //       <TableHead>Compartido</TableHead>
+        //       <TableHead className="text-right">Monto</TableHead>
+        //       <TableHead className="text-right">Acciones</TableHead>
+        //     </TableRow>
+        //   </TableHeader>
+        //   <TableBody>
+        //     <TableRow className="text-xs md:text-sm">
+        //       <TableCell className="sticky left-0 py-2 uppercase font-bold">
+        //         Gastos Fijos
+        //       </TableCell>
+        //     </TableRow>
+        //     {getNewExpenses()}
+        //     {expenseSummaries.map((item) => (
+        //       <TableRow key={item.id} className="text-xs md:text-sm">
+        //         <TableCell className="sticky left-0 bg-white capitalize">{item.expense.description.toLowerCase()}</TableCell>
+        //         <TableCell className='!capitalize'>{item.paymentType.name.toLowerCase()}</TableCell>
+        //         <TableCell className='!capitalize'>{item.paymentSource.name.toLowerCase()}</TableCell>
+        //         <TableCell>{item.dueDate ? formatLocaleDueDate(item.dueDate) : '-'}</TableCell>
+        //         <TableCell>{getStatusBadge(item)}</TableCell>
+        //         <TableCell className='!capitalize'>{item.expense.sharedWith.map((person) => person.name.toLowerCase()).join(' - ')}</TableCell>
+        //         <TableCell className="text-right">{formatCurrency(item.amount)}</TableCell>
+        //         <TableCell className="text-right flex flex-nowrap items-center justify-end">
+        //           <DropdownMenu>
+        //             <DropdownMenuTrigger asChild>
+        //               <Button disabled={isLoading} variant="ghost" size="icon">
+        //                 <MoreHorizontal className="h-4 w-4" />
+        //               </Button>
+        //             </DropdownMenuTrigger>
+        //             <DropdownMenuContent align="end">
+        //               <DropdownMenuLabel>Acciones</DropdownMenuLabel>
+        //               {item.paid ? (
+        //                 <>
+        //                   <DropdownMenuItem className="cursor-pointer" onClick={() => undoExpensePayment(item)}>
+        //                     Deshacer pago
+        //                   </DropdownMenuItem>
+        //                 </>
+        //               ) : (
+        //                 <>
+        //                   <DropdownMenuItem className="cursor-pointer">
+        //                     <Link
+        //                       className="w-full"
+        //                       href={`${PAGES_URL.EXPENSES.EDIT(item.expenseId)}?callbackUrl=/dashboard`}
+        //                     >
+        //                       Editar
+        //                     </Link>
+        //                   </DropdownMenuItem>
+        //                   <DropdownMenuItem className="cursor-pointer" onClick={() => dontPayExpense(item)}>
+        //                     Omitir pago
+        //                   </DropdownMenuItem>
+        //                   <DropdownMenuItem className="cursor-pointer" onClick={() => payExpense(item)}>
+        //                     Pagar
+        //                   </DropdownMenuItem>
+        //                 </>
+        //               )}
+        //             </DropdownMenuContent>
+        //           </DropdownMenu>
+        //         </TableCell>
+        //       </TableRow>
+        //     ))}
+        //     <TableRow className="text-xs md:text-sm">
+        //       <TableCell className="sticky left-0 py-2 uppercase font-bold">
+        //         Tarjetas crédito
+        //       </TableCell>
+        //     </TableRow>
+        //     {creditCardExpenseSummaries.map((item) => (
+        //       <TableRow key={item.id} className="text-xs md:text-sm">
+        //         <TableCell className="sticky left-0 bg-white">{item.creditCard.name}</TableCell>
+        //         <TableCell>{item.paymentType.name}</TableCell>
+        //         <TableCell>{item.paymentSource.name}</TableCell>
+        //         <TableCell>{item.dueDate ? formatLocaleDueDate(item.dueDate) : '-'}</TableCell>
+        //         <TableCell>{getStatusBadge(item)}</TableCell>
+        //         <TableCell>-</TableCell>
+        //         <TableCell className="text-right">{formatCurrency(item.amount)}</TableCell>
+        //         <TableCell className="text-right flex flex-nowrap items-center justify-end">
+        //           <DropdownMenu>
+        //             <DropdownMenuTrigger asChild>
+        //               <Button disabled={isLoading} variant="ghost" size="icon">
+        //                 <MoreHorizontal className="h-4 w-4" />
+        //               </Button>
+        //             </DropdownMenuTrigger>
+        //             <DropdownMenuContent align="end">
+        //               <DropdownMenuLabel>Acciones</DropdownMenuLabel>
+        //               <DropdownMenuItem className="cursor-pointer">
+        //                 <Link
+        //                   className="w-full"
+        //                   href={`${PAGES_URL.CREDIT_CARDS.SUMMARY.DETAIL(item.creditCardId, item.id)}`}
+        //                 >
+        //                   Ver detalle
+        //                 </Link>
+        //               </DropdownMenuItem>
+        //               {item.paid ? (
+        //                 <>
+        //                   <DropdownMenuItem className="cursor-pointer" onClick={() => undoCCExpensePayment(item)}>
+        //                     Deshacer pago
+        //                   </DropdownMenuItem>
+        //                 </>
+        //               ) : (
+        //                 <>
+        //                   <DropdownMenuItem className="cursor-pointer" onClick={() => payCCExpense(item)}>
+        //                     Pagar
+        //                   </DropdownMenuItem>
+        //                 </>
+        //               )}
+        //             </DropdownMenuContent>
+        //           </DropdownMenu>
+        //         </TableCell>
+        //       </TableRow>
+        //     ))}
+        //   </TableBody>
+        //   <TableFooter className="border-b border-b-gray-200">
+        //     <TableRow className="bg-white">
+        //       <TableCell className="py-0.5" colSpan={5} />
+        //       <TableCell className="py-0.5 pt-2 text-right">Total</TableCell>
+        //       <TableCell className="text-right py-0.5">{formatCurrency(getTotals().amount)}</TableCell>
+        //     </TableRow>
+        //     <TableRow className="bg-white">
+        //       <TableCell className="py-0.5" colSpan={5} />
+        //       <TableCell className="py-0.5 text-right">Pagado</TableCell>
+        //       <TableCell className="text-right py-0.5">{formatCurrency(getTotals().paid)}</TableCell>
+        //     </TableRow>
+        //     <TableRow className="bg-white">
+        //       <TableCell className="py-0.5" colSpan={5} />
+        //       <TableCell className="py-0.5 pb-2 text-right">No Pagado</TableCell>
+        //       <TableCell className="text-right py-0.5">{formatCurrency(getTotals().notPaid)}</TableCell>
+        //     </TableRow>
+        //   </TableFooter>
+        // </Table>
         <div>
           <p className="text-lg font-semibold mb-2">Gastos fijos</p>
           <div className="flex flex-col gap-2">
