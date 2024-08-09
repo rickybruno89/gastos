5ec05a54608bbd67c69eab57a6e716d5c97521dd
Ricky Bruno <ricardobruno_89@hotmail.com>
2024-08-09 18:14:59 -0300

    fix navbar

--- components/ui/dashboard/Navbar.tsx
+++ components/ui/dashboard/Navbar.tsx
@@ -52,7 +52,7 @@ export default function Navbar({ session }: { session: Session | null }) {
   }, [isMobileMenuOpen])
 
   return (
-    <Disclosure as="div" className="bg-gray-800">
+    <Disclosure as="div" className="bg-gray-800 fixed top-0 w-full z-50">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-14 items-center justify-between">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
